2c0ee492296527776ad3ebb5ddcf5e594084a80f
suha <suhaferdy@gmail.com>
2019-12-13 21:44:02 +0700

    ongoing latest chat message

---
+++ app/src/main/java/com/example/fproject1/item/ItemLatestMessage.kt
@@ -0,0 +1,18 @@
+package com.example.fproject1.item
+
+import com.example.fproject1.R
+import com.example.fproject1.model.Message
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.custom_latest_message.view.*
+
+class ItemLatestMessage(val message: Message) : Item<GroupieViewHolder>(){
+    override fun getLayout(): Int {
+        return R.layout.custom_latest_message
+    }
+
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        viewHolder.itemView.txt_latest_message.text = message.text
+    }
+
+}
